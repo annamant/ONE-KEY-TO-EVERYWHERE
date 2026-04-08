004aa01a0f447616469d1c6a798e43ecb70c668c
annamant <85039414+annamant@users.noreply.github.com>
2026-04-08 11:29:02 +0100

    Remove image elements and final CTA background

--- src/pages/public/LandingPage.tsx
+++ src/pages/public/LandingPage.tsx
@@ -1,7 +1,1 @@
-<img style={{ filter: 'brightness(1.12) saturate(1.08)', objectPosition: 'center' }} src={heroImage} alt="Hero Image" />
 
-<img style={{ filter: 'brightness(1.10) saturate(1.06)', objectPosition: 'center' }} src={ostuniImage} alt="Ostuni Image" />
-
-<img style={{ filter: 'brightness(1.08) saturate(1.05)' }} src={homeCardImage} alt="Home Card Image" />
-
-<div style={{ filter: 'brightness(1.10) saturate(1.06)', opacity: 0.50, position: 'absolute' }} className="final-cta-background"></div>
